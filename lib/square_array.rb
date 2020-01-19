235a73a940c4ce9ba27beee831d65149c2d826ab
Tyler Lai <lai.t.tyler@gmail.com>
2020-01-19 01:17:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,12 @@
 def square_array(array)
 
   counter = 0
-  # newArr = []
+  newArr = []
   while counter < array.length do
-    array[counter]**2
-    # newArr.push(array[count]**2)
+    newArr.push(array[counter]**2)
     counter +=1
   end
   
-  # newArr
+  newArr
   
 end
